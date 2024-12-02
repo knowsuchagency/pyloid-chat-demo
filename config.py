abd721f3d8aa5bd40a86120f8593debe9f2e8d35
Stephan Fitzpatrick <knowsuchagency@gmail.com>
2024-12-02 14:30:20 -0800

    separate config

---
+++ config.py
@@ -0,0 +1,29 @@
+from pathlib import Path
+import shelve
+
+import litellm
+
+
+class Config:
+    config_dir = Path.home() / ".demo"
+    config_path = config_dir / "config.db"
+    key = "api_key"
+
+    def __init__(self):
+        self.config_dir.mkdir(parents=True, exist_ok=True)
+        with shelve.open(str(self.config_path)) as db:
+            db.setdefault(self.key, "")
+
+    def get_api_key(self):
+        try:
+            with shelve.open(str(self.config_path)) as db:
+                result = db.get(self.key, "")
+                litellm.api_key = result
+                return result
+        except Exception:
+            return ""
+
+    def set_api_key(self, value):
+        with shelve.open(str(self.config_path)) as db:
+            db[self.key] = value
+            litellm.api_key = value
